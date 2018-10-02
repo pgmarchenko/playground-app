5ba05bc7eeb94192f554189b7bb43e4957f10665
Pavel Marchenko <pgmarchenko@gmail.com>
2018-10-02 14:45:20 +0300

    Removed storyboard

--- PlaygroundApp/AppDelegate.swift
+++ PlaygroundApp/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let rootVC = UIViewController()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
