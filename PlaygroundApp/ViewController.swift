05c9aef114071b566faa1dfa30c55c1dd1ed181a
Pavel Marchenko <pgmarchenko@gmail.com>
2018-10-02 14:34:06 +0300

    Initial Commit

---
+++ PlaygroundApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlaygroundApp
+//
+//  Created by Pavel Marchenko on 10/2/18.
+//  Copyright © 2018 pgmarchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
